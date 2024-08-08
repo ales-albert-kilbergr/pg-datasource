9d6ab8b1a8cf737e438490195e75c8fbbabf2b77
Ales Albert KILBERGR <ales.albert@kilbergr.eu>
2024-08-08 17:16:51 +0200

    feat: allow to register datasources in constructor

--- src/lib/datasource-registry.ts
+++ src/lib/datasource-registry.ts
@@ -3,6 +3,10 @@ import type { Datasource } from './datasource';
 export class DatasourceRegistry {
   private readonly map: Map<string, Datasource> = new Map<string, Datasource>();
 
+  public constructor(...datasource: Datasource[]) {
+    this.register(...datasource);
+  }
+
   public register(...datasource: Datasource[]): void {
     for (const ds of datasource) {
       this.map.set(ds.name, ds);
